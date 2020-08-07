88d2f1dbc05473b7245eee3ec79ef51c4e55bc16
박미현 <pmh2228@naver.com>
2020-08-07 18:37:29 +0900

    create default project

---
+++ pimanager/src/main/java/com/zizimee/api/pimanager/PimanagerApplication.java
@@ -0,0 +1,13 @@
+package com.zizimee.api.pimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimanagerApplication.class, args);
+	}
+
+}
