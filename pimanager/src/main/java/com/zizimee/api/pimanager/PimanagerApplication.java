01329cdf03fff5f550aa527709fb8adca8881e24
박미현 <pmh2228@naver.com>
2020-08-07 19:36:45 +0900

    h2 옵션 추가

--- pimanager/src/main/java/com/zizimee/api/pimanager/PimanagerApplication.java
+++ pimanager/src/main/java/com/zizimee/api/pimanager/PimanagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PimanagerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PimanagerApplication.class, args);
 	}
 
