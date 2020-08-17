fa450dfde713ff67b6ce946ba37747e12d15a072
박미현 <pmh2228@naver.com>
2020-08-18 04:42:56 +0900

    테스트 목적 권한 수정(로그인 과정 생략)

--- pimanager/src/main/java/com/zizimee/api/pimanager/config/SecurityConfig.java
+++ pimanager/src/main/java/com/zizimee/api/pimanager/config/SecurityConfig.java
@@ -19,23 +19,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        /*
         http
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf()
                     .disable()           //개발 편의성 위해 csrf 토큰 검사 비활성화
+                .headers().frameOptions().disable()
+                .and()
                 .httpBasic()
-                    .disable()         //basic authentication 비활성화
+                    .disable()  //basic authentication 비활성화
                 .authorizeRequests() //URL별 권한 관리 설정 옵션 시작점(인가에 관련한 처리)
+                    .antMatchers("/login/*")
+                        .authenticated()
                     .anyRequest()  //설정 값(antMatchers) 이외 나머지 URL
-                        .authenticated()  //인증된 사용자(로그인 한 사용자)들만 허용
+                        .permitAll()  //인증된 사용자(로그인 한 사용자)들만 허용
         ;
 
         //security는 id-pw기반인증
         //username = id
         //session disable
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+         */
+
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll();
     }
 
 }
