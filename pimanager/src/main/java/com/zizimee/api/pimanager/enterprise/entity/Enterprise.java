4214673716d08c68fb357c7757001180223ccd6b
박미현 <pmh2228@naver.com>
2020-08-27 01:55:18 +0900

    refactor: string type annotation 수정

--- pimanager/src/main/java/com/zizimee/api/pimanager/enterprise/entity/Enterprise.java
+++ pimanager/src/main/java/com/zizimee/api/pimanager/enterprise/entity/Enterprise.java
@@ -1,36 +1,53 @@
-package com.zizimee.api.pimanager.Enterprise.Entity;
+package com.zizimee.api.pimanager.enterprise.entity;
 
-
+import com.zizimee.api.pimanager.report.entity.Report;
+import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Enterprise {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idEnterprise;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
-    private String domainAddress;
-
-    @Column(nullable = false)
-    private String id;
+    private String signUpId;
 
     @Column(nullable = false)
     private String password;
 
+    private String domainAddress;
+    private String registerNmb;
+
+    @ElementCollection
+    @CollectionTable(name = "CONSENT_LIST", joinColumns = @JoinColumn(name="ENTERPRISE_ID"))
+    @Column(name="CONSENT_DETAIL")
+    private List<String> consents = new ArrayList<>();
+
+    @OneToMany(mappedBy="enterprise")
+    private List<Report> reports;
+
+
     @Builder
-    public Enterprise(String name, String domainAddress, String id, String password){
+    public Enterprise(String name, String signUpId, String password, String domainAddress, String registerNmb, String profileImg) {
         this.name = name;
-        this.domainAddress = domainAddress;
-        this.id = id;
+        this.signUpId = signUpId;
         this.password = password;
+        this.domainAddress = domainAddress;
+        this.registerNmb = registerNmb;
     }
+
+
 }
