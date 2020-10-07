b763065fa7b06afc80c83bca2656c073f4ca5157
박미현 <pmh2228@naver.com>
2020-10-08 02:17:25 +0900

    refactor:#15 dto에서 providerType 필드 삭제

--- pimanager/src/main/java/com/zizimee/api/pimanager/user/dto/RequestSignUpDto.java
+++ pimanager/src/main/java/com/zizimee/api/pimanager/user/dto/RequestSignUpDto.java
@@ -7,9 +7,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RequestSignUpDto {
 
-    //오프라인일 경우 사용자 대신 google api 부르기 바란다면 -> access token도 서버로 받아오기
-
-    private ProviderType provider;
     private String idToken;
 
 }
